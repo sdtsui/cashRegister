da2f42920b3a534789585956a1072fbcd354d81d
sdtsui <danielt213@gmail.com>
2015-12-06 20:21:23 -0800

    added scanItem, implementing validation logic before updateTransaction

--- CashRegister.js
+++ CashRegister.js
@@ -8,110 +8,46 @@ let DiscountsController = require("./services/Discounts.js");
 
 class CashRegister {
   constuctor() {
-    this.currentTransaction = null;
 
   } 
 
-  //findOrCreate Logic
-  startTransaction() {
-    //if there is no current transaction
-    //  create a new one, insert as pending
-    //else : there is a current transaction
-    //  check if it matches the current DB
-    //  if it does:
-    //    update it, send to DB
-    //  if it does not:
-    //    take the new one, update it, send to DB
-    //cb ("success")?
+  scanItem(itemID, cb) {
+    InventoryController.getDetails(itemID)
+      .then((err, item) => {
+        cb(err, item);
+        if (err) {
+          throw new Error('errorText');
+        }
+      });
+  }
 
-    if (this.current)
-    if (!this.currentTransaction) {
-      this.currentTransaction = {
-        ID : null,
-        completed: false,
-        discountApplied: false,
-        itemList: [], // {ID, unit, quant, rate}
-        currentDiscounts: [], // ID
-        total: undefined,
-      }
+  scanDiscount() {
 
-      TransactionsController.insertNew(this.currentTransaction);
-    } else {
-      
-    }
-      TransactionsController.findOne(this.currentTransaction, (err, transaction) => {
-        //if error
-        if (!!err) {
-          console.log("Error in CashRegister, from TransactionsController: ",
-            err
-            );
-          cb(err, null);
-        } else {
-          if (!!transaction) {
-            //found, already in service
+  }
 
-          } else {
-            //none found, must create new
+
+  addItem(trans_ID, item_ID, quant, cb) {
+    //only if valid, we can do below??
+
+    TransactionsController.getTransaction(trans_ID,
+      (err, transactionJSON) => {
+        //transactionJSON might be null
+        if (!!transactionJSON) {
+          let trans_obj = JSON.parse(transactionJSON);
+          if (CashRegister.transactionJSON_isValid(trans_obj)){//???
+            trans_obj.itemList.push(item_ID, quant);
+            CashRegister.updateTransaction(trans_obj, (err, trans) => {
+              cb(err, trans);
+            });
           }
-
-        } 
-      })
-    }
-    this.currentTransaction.itemList.push(item);
-    //handle duplicates, etc? 
-    //
-    cb(null, this.currentTransaction); //currentList?
-    cb(null, "Success");
-
-
+        }
+      }
+      )
   }
 
-  //no return value, only send complete transaction to DB
-  endTransaction() {
-    //resetTransaction
-  }
 
-  /**
-   * inputs: item paramaters in strings and numers
-   * outputs: none
-   * side effects: adds an item-quantity-unit tuple to the current transaction
-   * @return {[type]} [description]
-   */
-  scan(itemID, unitID = 00, quantity = 1, cb) {
-    //if there is no active transaction, throw
-    //if there is one, add to it.
+  startTransaction() {
 
-    // (two steps: scan, punch in quantity)
-    // scan item, automatically add it as one unit;
-    //  -edit item afterwards
-
-    //scan item
-    //enter quantity and unit
-    //add to card
-    //
-    //scanAndAdd Method
-    if (!itemID || typeof itemID !== number) { 
-      return new Error("Scan failed: Invalid itemID.");
-    }
-    //find price data, update
-    let rate = PricingController.getRate(unitID, quantity);
-    let itemCost = rate * quantity;
-  }
-
-  /**
-   * inputs: discountID
-   * outputs: none
-   *
-   * side effects: add a discount to the transaction
-   *
-   * Adding discount won't affect any of the transaction's properties.
-   * Discounts get applied at the last possible moment.
-   * Only when a total is needed, the discount is applied.
-   * @return {[type]} [description]
-   */
-  applyDiscount(discountID) {
-    //if there is no current transaction: return error
-    
   }
 
   /**
@@ -140,6 +76,66 @@ class CashRegister {
    * Assumption / tech debt: maybe a transaction should be its own
    * type-safe list. Too much abstraction?
    */
+  
+  static updateTransaction(id, JSON) {
+    //will use validate
+    //could update completed?
+  }
+
+  static transactionJSON_isValid(transaction) {
+    //this is a transaction that exists in the DB
+    
+    //transaction doesn't exist, defensive
+    TransactionController.getTransaction(transaction.id
+      (err, trans) => {
+        InventoryController.checkIfInStock(transaction.itemList,
+          (err, allInStock) => {
+            if(allInStock) {
+              DiscountsController.getDiscount(transaction.currentDiscounts,
+                () => {
+                });
+            }
+          }
+          );
+      }
+      );
+    TransactionController.getTransaction(transaction.id)
+      .then((err, trans) => {
+        if (!err ) {
+          InventoryController.checkIfItemsInStock(transaction.itemList);
+        }
+      })
+      .then((err, allInStock) => {
+        if (!err && !!allInStock) {
+          DiscountsController.getDiscounts(transaction.currentDiscounts);
+        }
+      })
+      .then((err, allValid) => {
+
+      });
+      //do all checks
+
+    //coupon doesn't exist
+    //item is out of stock
+    validationCalls.map()
+
+    validationCalls.done()
+
+    //async, if all complete,
+    //
+  }
+
+  static getTotalCost(id) {
+
+  }
+
+  static getListOfItems(id) {
+
+  }
+
+
+
+
 }
 
 
